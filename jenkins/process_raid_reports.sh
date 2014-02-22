5d012994e0a6bdf62d05cb1238235a7e042a4e94
philoptik <philoptik@gmail.com>
2014-02-22 15:06:04 +0100

    Archive raid reports to git repository.

--- jenkins/process_raid_reports.sh
+++ jenkins/process_raid_reports.sh
@@ -6,3 +6,10 @@ cp /simc/reports/Raid_T16H.html /simc/reports/archive_html/${NOW}_Raid_T16H.html
 
 cp /simc/reports/Raid_T15H.txt /simc/reports/archive_txt/${NOW}_Raid_T15H.txt
 cp /simc/reports/Raid_T16H.txt /simc/reports/archive_txt/${NOW}_Raid_T16H.txt
+
+# Work on archive git repository
+cd /simc/reports
+git add -A
+git commit -m"Reports from ${NOW}"
+git pull
+git push
