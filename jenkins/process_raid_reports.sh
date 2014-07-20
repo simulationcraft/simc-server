04e4154b10db901bb4ec1d1afba7c46d8ca3981f
philoptik <philoptik@gmail.com>
2014-07-20 10:31:38 +0200

    gzip archived raid reports

--- jenkins/process_raid_reports.sh
+++ jenkins/process_raid_reports.sh
@@ -5,7 +5,9 @@ cp /simc/reports/Raid_T15H.html /simc/reports/archive_html/${NOW}_Raid_T15H.html
 cp /simc/reports/Raid_T16H.html /simc/reports/archive_html/${NOW}_Raid_T16H.html
 
 cp /simc/reports/Raid_T15H.txt /simc/reports/archive_txt/${NOW}_Raid_T15H.txt
+gzip /simc/reports/archive_txt/${NOW}_Raid_T15H.txt
 cp /simc/reports/Raid_T16H.txt /simc/reports/archive_txt/${NOW}_Raid_T16H.txt
+gzip /simc/reports/archive_txt/${NOW}_Raid_T16H.txt
 
 # Work on archive git repository
 cd /simc/reports
