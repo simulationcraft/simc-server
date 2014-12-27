3fa2ec4a0cd9a4458bc9ac660ea49123fb348ce1
Alex <s.alexlowery@gmail.com>
2014-12-27 13:47:50 -0500

    update server scripts

--- jenkins/process_raid_reports.sh
+++ jenkins/process_raid_reports.sh
@@ -2,17 +2,23 @@
 
 NOW=$(date +%s)
 
-cp /simc/reports/Raid_T16M.html /simc/reports/archive_html/${NOW}_Raid_T16M.html
-gzip /simc/reports/archive_html/${NOW}_Raid_T16M.html
-
 cp /simc/reports/Raid_T17M.html /simc/reports/archive_html/${NOW}_Raid_T17M.html
 gzip /simc/reports/archive_html/${NOW}_Raid_T17M.html
 
-cp /simc/reports/Raid_T16M.txt /simc/reports/archive_txt/${NOW}_Raid_T16M.txt
-gzip /simc/reports/archive_txt/${NOW}_Raid_T16M.txt
+cp /simc/reports/Raid_T17M.html /simc/reports/archive_html/${NOW}_Raid_T17N.html
+gzip /simc/reports/archive_html/${NOW}_Raid_T17N.html
+
+cp /simc/reports/Raid_T17M.html /simc/reports/archive_html/${NOW}_Raid_T17P.html
+gzip /simc/reports/archive_html/${NOW}_Raid_T17P.html
 
 cp /simc/reports/Raid_T17M.txt /simc/reports/archive_txt/${NOW}_Raid_T17M.txt
 gzip /simc/reports/archive_txt/${NOW}_Raid_T17M.txt
+
+cp /simc/reports/Raid_T17M.txt /simc/reports/archive_txt/${NOW}_Raid_T17N.txt
+gzip /simc/reports/archive_txt/${NOW}_Raid_T17N.txt
+
+cp /simc/reports/Raid_T17M.txt /simc/reports/archive_txt/${NOW}_Raid_T17P.txt
+gzip /simc/reports/archive_txt/${NOW}_Raid_T17P.txt
 
 # Work on archive git repository
 cd /simc/reports
