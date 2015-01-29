553735520eb71944b88ff125f26153caed6a8cfc
Alex <s.alexlowery@gmail.com>
2015-01-29 04:01:03 -0500

    swap aoe sims to T17H, as it is more representative of gear that people raid with now.

--- jenkins/process_raid_reports.sh
+++ jenkins/process_raid_reports.sh
@@ -11,11 +11,11 @@ gzip /simc/reports/archive_html/${NOW}_Raid_T17N.html
 cp /simc/reports/Raid_T17P.html /simc/reports/archive_html/${NOW}_Raid_T17P.html
 gzip /simc/reports/archive_html/${NOW}_Raid_T17P.html
 
-cp /simc/reports/Raid_T17N_AOE.html /simc/reports/archive_html/${NOW}_Raid_T17N_AOE.html
-gzip /simc/reports/archive_html/${NOW}_Raid_T17N_AOE.html
+cp /simc/reports/Raid_T17H_AOE.html /simc/reports/archive_html/${NOW}_Raid_T17H_AOE.html
+gzip /simc/reports/archive_html/${NOW}_Raid_T17H_AOE.html
 
-cp /simc/reports/Raid_T17N_AddWaves.html /simc/reports/archive_html/${NOW}_Raid_T17N_AddWaves.html
-gzip /simc/reports/archive_html/${NOW}_Raid_T17N_AddWaves.html
+cp /simc/reports/Raid_T17H_AddWaves.html /simc/reports/archive_html/${NOW}_Raid_T17H_AddWaves.html
+gzip /simc/reports/archive_html/${NOW}_Raid_T17H_AddWaves.html
 
 cp /simc/reports/Raid_T17M.txt /simc/reports/archive_txt/${NOW}_Raid_T17M.txt
 gzip /simc/reports/archive_txt/${NOW}_Raid_T17M.txt
@@ -26,11 +26,11 @@ gzip /simc/reports/archive_txt/${NOW}_Raid_T17N.txt
 cp /simc/reports/Raid_T17P.txt /simc/reports/archive_txt/${NOW}_Raid_T17P.txt
 gzip /simc/reports/archive_txt/${NOW}_Raid_T17P.txt
 
-cp /simc/reports/Raid_T17N_AOE.txt /simc/reports/archive_txt/${NOW}_Raid_T17N_AOE.txt
-gzip /simc/reports/archive_txt/${NOW}_Raid_T17N_AOE.txt
+cp /simc/reports/Raid_T17H_AOE.txt /simc/reports/archive_txt/${NOW}_Raid_T17H_AOE.txt
+gzip /simc/reports/archive_txt/${NOW}_Raid_T17H_AOE.txt
 
-cp /simc/reports/_Raid_T17N_AddWaves.txt /simc/reports/archive_txt/${NOW}_Raid_T17N_AddWaves.txt
-gzip /simc/reports/archive_txt/${NOW}_Raid_T17N_AddWaves.txt
+cp /simc/reports/_Raid_T17H_AddWaves.txt /simc/reports/archive_txt/${NOW}_Raid_T17H_AddWaves.txt
+gzip /simc/reports/archive_txt/${NOW}_Raid_T17H_AddWaves.txt
 
 # Work on archive git repository
 cd /simc/reports
