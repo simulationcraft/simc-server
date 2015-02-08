cb81ddd868955716f0973aa2bb7ce5bd4e3254bb
Alex <s.alexlowery@gmail.com>
2015-02-08 11:03:48 -0500

    remove typo

--- jenkins/process_raid_reports.sh
+++ jenkins/process_raid_reports.sh
@@ -29,7 +29,7 @@ gzip /simc/reports/archive_txt/${NOW}_Raid_T17P.txt
 cp /simc/reports/Raid_T17H_AOE.txt /simc/reports/archive_txt/${NOW}_Raid_T17H_AOE.txt
 gzip /simc/reports/archive_txt/${NOW}_Raid_T17H_AOE.txt
 
-cp /simc/reports/_Raid_T17H_AddWaves.txt /simc/reports/archive_txt/${NOW}_Raid_T17H_AddWaves.txt
+cp /simc/reports/Raid_T17H_AddWaves.txt /simc/reports/archive_txt/${NOW}_Raid_T17H_AddWaves.txt
 gzip /simc/reports/archive_txt/${NOW}_Raid_T17H_AddWaves.txt
 
 # Work on archive git repository
