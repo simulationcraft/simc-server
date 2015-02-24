7fb12895495e5e9ca622d205cf0c84031a66f016
Alex <s.alexlowery@gmail.com>
2015-02-24 14:16:33 -0500

    process raid reports update

--- jenkins/process_raid_reports.sh
+++ jenkins/process_raid_reports.sh
@@ -8,8 +8,8 @@ gzip /simc/reports/archive_html/${NOW}_Raid_T17M.html
 cp /simc/reports/Raid_T17N.html /simc/reports/archive_html/${NOW}_Raid_T17N.html
 gzip /simc/reports/archive_html/${NOW}_Raid_T17N.html
 
-cp /simc/reports/Raid_T17P.html /simc/reports/archive_html/${NOW}_Raid_T17P.html
-gzip /simc/reports/archive_html/${NOW}_Raid_T17P.html
+cp /simc/reports/Raid_T17P.html /simc/reports/archive_html/${NOW}_Raid_T17H.html
+gzip /simc/reports/archive_html/${NOW}_Raid_T17H.html
 
 cp /simc/reports/Raid_T17H_AOE.html /simc/reports/archive_html/${NOW}_Raid_T17H_AOE.html
 gzip /simc/reports/archive_html/${NOW}_Raid_T17H_AOE.html
@@ -23,8 +23,8 @@ gzip /simc/reports/archive_txt/${NOW}_Raid_T17M.txt
 cp /simc/reports/Raid_T17N.txt /simc/reports/archive_txt/${NOW}_Raid_T17N.txt
 gzip /simc/reports/archive_txt/${NOW}_Raid_T17N.txt
 
-cp /simc/reports/Raid_T17P.txt /simc/reports/archive_txt/${NOW}_Raid_T17P.txt
-gzip /simc/reports/archive_txt/${NOW}_Raid_T17P.txt
+cp /simc/reports/Raid_T17P.txt /simc/reports/archive_txt/${NOW}_Raid_T17H.txt
+gzip /simc/reports/archive_txt/${NOW}_Raid_T17H.txt
 
 cp /simc/reports/Raid_T17H_AOE.txt /simc/reports/archive_txt/${NOW}_Raid_T17H_AOE.txt
 gzip /simc/reports/archive_txt/${NOW}_Raid_T17H_AOE.txt
