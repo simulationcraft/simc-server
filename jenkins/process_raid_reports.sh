051c2499fa27738e79dbd6cd97346b19c3c3100f
Navv <navv1234@gmail.com>
2015-06-23 22:56:23 +0300

    Update daily raid report generation to T18.

--- jenkins/process_raid_reports.sh
+++ jenkins/process_raid_reports.sh
@@ -2,33 +2,36 @@
 
 NOW=$(date +%s)
 
-cp /simc/reports/Raid_T17M.html /simc/reports/archive_html/${NOW}_Raid_T17M.html
-gzip /simc/reports/archive_html/${NOW}_Raid_T17M.html
+#cp /simc/reports/Raid_T17M.html /simc/reports/archive_html/${NOW}_Raid_T17M.html
+#gzip /simc/reports/archive_html/${NOW}_Raid_T17M.html
 
-cp /simc/reports/Raid_T17N.html /simc/reports/archive_html/${NOW}_Raid_T17N.html
-gzip /simc/reports/archive_html/${NOW}_Raid_T17N.html
+#cp /simc/reports/Raid_T17N.html /simc/reports/archive_html/${NOW}_Raid_T17N.html
+#gzip /simc/reports/archive_html/${NOW}_Raid_T17N.html
 
-cp /simc/reports/Raid_T17P.html /simc/reports/archive_html/${NOW}_Raid_T17H.html
-gzip /simc/reports/archive_html/${NOW}_Raid_T17H.html
+#cp /simc/reports/Raid_T17P.html /simc/reports/archive_html/${NOW}_Raid_T17H.html
+#gzip /simc/reports/archive_html/${NOW}_Raid_T17H.html
 
-cp /simc/reports/Raid_T17H_AOE.html /simc/reports/archive_html/${NOW}_Raid_T17H_AOE.html
-gzip /simc/reports/archive_html/${NOW}_Raid_T17H_AOE.html
+#cp /simc/reports/Raid_T17M.txt /simc/reports/archive_txt/${NOW}_Raid_T17M.txt
+#gzip /simc/reports/archive_txt/${NOW}_Raid_T17M.txt
 
-cp /simc/reports/Raid_T17H_AddWaves.html /simc/reports/archive_html/${NOW}_Raid_T17H_AddWaves.html
-gzip /simc/reports/archive_html/${NOW}_Raid_T17H_AddWaves.html
+#cp /simc/reports/Raid_T17P.txt /simc/reports/archive_txt/${NOW}_Raid_T17H.txt
+#gzip /simc/reports/archive_txt/${NOW}_Raid_T17H.txt
 
-cp /simc/reports/Raid_T17M.txt /simc/reports/archive_txt/${NOW}_Raid_T17M.txt
-gzip /simc/reports/archive_txt/${NOW}_Raid_T17M.txt
+#cp /simc/reports/Raid_T17H_AOE.txt /simc/reports/archive_txt/${NOW}_Raid_T17H_AOE.txt
+#gzip /simc/reports/archive_txt/${NOW}_Raid_T17H_AOE.txt
 
-cp /simc/reports/Raid_T17N.txt /simc/reports/archive_txt/${NOW}_Raid_T17N.txt
-gzip /simc/reports/archive_txt/${NOW}_Raid_T17N.txt
+#cp /simc/reports/Raid_T17H_AddWaves.txt /simc/reports/archive_txt/${NOW}_Raid_T17H_AddWaves.txt
+#gzip /simc/reports/archive_txt/${NOW}_Raid_T17H_AddWaves.txt
 
-cp /simc/reports/Raid_T17P.txt /simc/reports/archive_txt/${NOW}_Raid_T17H.txt
-gzip /simc/reports/archive_txt/${NOW}_Raid_T17H.txt
+cp /simc/reports/Raid_T18H_AOE.html /simc/reports/archive_html/${NOW}_Raid_T18H_AOE.html
+gzip /simc/reports/archive_html/${NOW}_Raid_T18H_AOE.html
 
-cp /simc/reports/Raid_T17H_AOE.txt /simc/reports/archive_txt/${NOW}_Raid_T17H_AOE.txt
-gzip /simc/reports/archive_txt/${NOW}_Raid_T17H_AOE.txt
+cp /simc/reports/Raid_T18H_AddWaves.html /simc/reports/archive_html/${NOW}_Raid_T18H_AddWaves.html
+gzip /simc/reports/archive_html/${NOW}_Raid_T18H_AddWaves.html
 
-cp /simc/reports/Raid_T17H_AddWaves.txt /simc/reports/archive_txt/${NOW}_Raid_T17H_AddWaves.txt
-gzip /simc/reports/archive_txt/${NOW}_Raid_T17H_AddWaves.txt
+cp /simc/reports/Raid_T18N.txt /simc/reports/archive_txt/${NOW}_Raid_T18N.txt
+gzip /simc/reports/archive_txt/${NOW}_Raid_T18N.txt
+
+cp /simc/reports/Raid_T18H.txt /simc/reports/archive_txt/${NOW}_Raid_T18H.txt
+gzip /simc/reports/archive_txt/${NOW}_Raid_T18H.txt
 
--- jenkins/run_raid_reports.sh
+++ jenkins/run_raid_reports.sh
@@ -28,12 +28,12 @@ SIMC_CLI_PATH="/var/lib/jenkins/jobs/simc-raid-reports/workspace/engine"
 SIMC_PROFILES_PATH="/var/lib/jenkins/jobs/simc-raid-reports/workspace/profiles"
 SIMC_OUTPUT_PATH="/simc/reports"
 
-${SIMC_CLI_PATH}/simc Raid_T17N.simc iterations=${simc_iterations} html=${SIMC_OUTPUT_PATH}/Raid_T17N.html threads=${available_cpus} ${other_options} > ${SIMC_OUTPUT_PATH}/Raid_T17N.txt
-${SIMC_CLI_PATH}/simc Raid_T17H.simc iterations=${simc_iterations} html=${SIMC_OUTPUT_PATH}/Raid_T17H.html threads=${available_cpus} ${other_options} > ${SIMC_OUTPUT_PATH}/Raid_T17H.txt
-${SIMC_CLI_PATH}/simc Raid_T17M.simc iterations=${simc_iterations} html=${SIMC_OUTPUT_PATH}/Raid_T17M.html threads=${available_cpus} ${other_options} > ${SIMC_OUTPUT_PATH}/Raid_T17M.txt
+${SIMC_CLI_PATH}/simc enable_highcharts=1 Raid_T18N.simc iterations=${simc_iterations} html=${SIMC_OUTPUT_PATH}/Raid_T18N.html threads=${available_cpus} ${other_options} > ${SIMC_OUTPUT_PATH}/Raid_T18N.txt
+${SIMC_CLI_PATH}/simc enable_highcharts=1 Raid_T18H.simc iterations=${simc_iterations} html=${SIMC_OUTPUT_PATH}/Raid_T18H.html threads=${available_cpus} ${other_options} > ${SIMC_OUTPUT_PATH}/Raid_T18H.txt
+#${SIMC_CLI_PATH}/simc Raid_T17M.simc iterations=${simc_iterations} html=${SIMC_OUTPUT_PATH}/Raid_T17M.html threads=${available_cpus} ${other_options} > ${SIMC_OUTPUT_PATH}/Raid_T17M.txt
 
 # Aoe Sim
 aoe_options="enemy=enemy2 actions=auto_attack,damage=0 enemy=enemy3 actions=auto_attack,damage=0 enemy=enemy4 actions=auto_attack,damage=0"
 aoe_options2="raid_events+=/adds,count=8,first=15,cooldown=45,duration=15"
-${SIMC_CLI_PATH}/simc Raid_T17M_AOE.simc ${aoe_options} iterations=${simc_iterations} html=${SIMC_OUTPUT_PATH}/Raid_T17M_AOE.html threads=${available_cpus} ${other_options} > ${SIMC_OUTPUT_PATH}/Raid_T17M_AOE.txt
-${SIMC_CLI_PATH}/simc Raid_T17M_Waves.simc ${aoe_options2} iterations=${simc_iterations} html=${SIMC_OUTPUT_PATH}/Raid_T17M_AddWaves.html threads=${available_cpus} ${other_options} > ${SIMC_OUTPUT_PATH}/Raid_T17M_AddWaves.txt
+${SIMC_CLI_PATH}/simc enable_highcharts=1 Raid_T18H_AOE.simc ${aoe_options} iterations=${simc_iterations} html=${SIMC_OUTPUT_PATH}/Raid_T18H_AOE.html threads=${available_cpus} ${other_options} > ${SIMC_OUTPUT_PATH}/Raid_T18H_AOE.txt
+${SIMC_CLI_PATH}/simc enable_highcharts=1 Raid_T18H_Waves.simc ${aoe_options2} iterations=${simc_iterations} html=${SIMC_OUTPUT_PATH}/Raid_T18H_AddWaves.html threads=${available_cpus} ${other_options} > ${SIMC_OUTPUT_PATH}/Raid_T18H_AddWaves.txt
