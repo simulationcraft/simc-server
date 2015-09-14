904a2ca2d4f44147991c76539f2e25a7d618fa32
Serge Camille <sergecamille@gmail.com>
2015-09-14 22:58:33 +0200

    add documentation build script.

---
+++ jenkins/build_doc.sh
@@ -0,0 +1,13 @@
+DOXYGEN_EXEC="doxygen"
+DOC_DIR="./doc"
+DOXYGEN_OUT="doxygen/"
+WEB_OUT="/simc/doxygen"
+
+# cd into doc folder
+cd ${DOC_DIR}
+
+# build doc
+${DOXYGEN_EXEC}
+
+# copy doc to webserver
+cp ${DOXYGEN_OUT} ${WEB_OUT}
