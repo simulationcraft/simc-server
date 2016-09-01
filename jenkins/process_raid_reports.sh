b2c2d2c382967c6eb82028fe47ae0c6ff37cb079
Collision <s.alexlowery@gmail.com>
2016-09-01 05:13:41 -0400

    Update raid reports to use t19p

--- jenkins/process_raid_reports.sh
+++ jenkins/process_raid_reports.sh
@@ -2,31 +2,7 @@
 
 NOW=$(date -Iseconds -u)
 
-REPORT=Raid_T18N
-cp /simc/reports/${REPORT}.html /simc/reports/archive_html/${NOW}_${REPORT}.html
-gzip /simc/reports/archive_html/${NOW}_${REPORT}.html
-cp /simc/reports/${REPORT}.txt /simc/reports/archive_txt/${NOW}_${REPORT}.txt
-gzip /simc/reports/archive_txt/${NOW}_${REPORT}.txt
-
-REPORT=Raid_T18H
-cp /simc/reports/${REPORT}.html /simc/reports/archive_html/${NOW}_${REPORT}.html
-gzip /simc/reports/archive_html/${NOW}_${REPORT}.html
-cp /simc/reports/${REPORT}.txt /simc/reports/archive_txt/${NOW}_${REPORT}.txt
-gzip /simc/reports/archive_txt/${NOW}_${REPORT}.txt
-
-REPORT=Raid_T18M
-cp /simc/reports/${REPORT}.html /simc/reports/archive_html/${NOW}_${REPORT}.html
-gzip /simc/reports/archive_html/${NOW}_${REPORT}.html
-cp /simc/reports/${REPORT}.txt /simc/reports/archive_txt/${NOW}_${REPORT}.txt
-gzip /simc/reports/archive_txt/${NOW}_${REPORT}.txt
-
-REPORT=Raid_T18H_AddWaves
-cp /simc/reports/${REPORT}.html /simc/reports/archive_html/${NOW}_${REPORT}.html
-gzip /simc/reports/archive_html/${NOW}_${REPORT}.html
-cp /simc/reports/${REPORT}.txt /simc/reports/archive_txt/${NOW}_${REPORT}.txt
-gzip /simc/reports/archive_txt/${NOW}_${REPORT}.txt
-
-REPORT=Raid_T18H_AOE
+REPORT=Raid_T19P
 cp /simc/reports/${REPORT}.html /simc/reports/archive_html/${NOW}_${REPORT}.html
 gzip /simc/reports/archive_html/${NOW}_${REPORT}.html
 cp /simc/reports/${REPORT}.txt /simc/reports/archive_txt/${NOW}_${REPORT}.txt
--- jenkins/run_raid_reports.sh
+++ jenkins/run_raid_reports.sh
@@ -15,7 +15,7 @@ if [ -n "${cpu_count_override}" ]; then
 fi
 
 # Iterations
-simc_iterations=7500
+simc_iterations=15000
 if [ -n "${simc_iterations_override}" ]; then
   simc_iterations=${simc_iterations_override}
 fi
@@ -28,12 +28,12 @@ SIMC_CLI_PATH="/var/lib/jenkins/jobs/simc-raid-reports/workspace/engine"
 SIMC_PROFILES_PATH="/var/lib/jenkins/jobs/simc-raid-reports/workspace/profiles"
 SIMC_OUTPUT_PATH="/simc/reports"
 
-${SIMC_CLI_PATH}/simc enable_highcharts=1 Raid_T18N.simc iterations=${simc_iterations} html=${SIMC_OUTPUT_PATH}/Raid_T18N.html threads=${available_cpus} ${other_options} > ${SIMC_OUTPUT_PATH}/Raid_T18N.txt
-${SIMC_CLI_PATH}/simc enable_highcharts=1 Raid_T18H.simc iterations=${simc_iterations} html=${SIMC_OUTPUT_PATH}/Raid_T18H.html threads=${available_cpus} ${other_options} > ${SIMC_OUTPUT_PATH}/Raid_T18H.txt
-${SIMC_CLI_PATH}/simc enable_highcharts=1 Raid_T18M.simc iterations=${simc_iterations} html=${SIMC_OUTPUT_PATH}/Raid_T18M.html threads=${available_cpus} ${other_options} > ${SIMC_OUTPUT_PATH}/Raid_T18M.txt
+${SIMC_CLI_PATH}/simc enable_highcharts=1 Raid_T19P.simc iterations=${simc_iterations} html=${SIMC_OUTPUT_PATH}/Raid_T19P.html threads=${available_cpus} ${other_options} > ${SIMC_OUTPUT_PATH}/Raid_T19P.txt
+#${SIMC_CLI_PATH}/simc enable_highcharts=1 Raid_T18H.simc iterations=${simc_iterations} html=${SIMC_OUTPUT_PATH}/Raid_T18H.html threads=${available_cpus} ${other_options} > #${SIMC_OUTPUT_PATH}/Raid_T18H.txt
+#${SIMC_CLI_PATH}/simc enable_highcharts=1 Raid_T18M.simc iterations=${simc_iterations} html=${SIMC_OUTPUT_PATH}/Raid_T18M.html threads=${available_cpus} ${other_options} > #${SIMC_OUTPUT_PATH}/Raid_T18M.txt
 
 # Aoe Sim
-aoe_options="enemy=enemy2 actions=auto_attack,damage=0 enemy=enemy3 actions=auto_attack,damage=0 enemy=enemy4 actions=auto_attack,damage=0"
-aoe_options2="raid_events+=/adds,count=8,first=15,cooldown=45,duration=15"
-${SIMC_CLI_PATH}/simc enable_highcharts=1 Raid_T18H_AOE.simc ${aoe_options} iterations=${simc_iterations} html=${SIMC_OUTPUT_PATH}/Raid_T18H_AOE.html threads=${available_cpus} ${other_options} > ${SIMC_OUTPUT_PATH}/Raid_T18H_AOE.txt
-${SIMC_CLI_PATH}/simc enable_highcharts=1 Raid_T18H_Waves.simc ${aoe_options2} iterations=${simc_iterations} html=${SIMC_OUTPUT_PATH}/Raid_T18H_AddWaves.html threads=${available_cpus} ${other_options} > ${SIMC_OUTPUT_PATH}/Raid_T18H_AddWaves.txt
+#aoe_options="enemy=enemy2 actions=auto_attack,damage=0 enemy=enemy3 actions=auto_attack,damage=0 enemy=enemy4 actions=auto_attack,damage=0"
+#aoe_options2="raid_events+=/adds,count=8,first=15,cooldown=45,duration=15"
+#${SIMC_CLI_PATH}/simc enable_highcharts=1 Raid_T18H_AOE.simc ${aoe_options} iterations=${simc_iterations} html=${SIMC_OUTPUT_PATH}/Raid_T18H_AOE.html threads=${available_cpus} #${other_options} > ${SIMC_OUTPUT_PATH}/Raid_T18H_AOE.txt
+#${SIMC_CLI_PATH}/simc enable_highcharts=1 Raid_T18H_Waves.simc ${aoe_options2} iterations=${simc_iterations} html=${SIMC_OUTPUT_PATH}/Raid_T18H_AddWaves.html #threads=${available_cpus} ${other_options} > ${SIMC_OUTPUT_PATH}/Raid_T18H_AddWaves.txt
