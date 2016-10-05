dafc84c2208d89c86c927720e5c7ae3a8d95e31d
Collision <s.alexlowery@gmail.com>
2016-10-04 20:06:59 -0400

    correct mistake

--- jenkins/run_raid_reports.sh
+++ jenkins/run_raid_reports.sh
@@ -29,8 +29,8 @@ SIMC_PROFILES_PATH="/var/lib/jenkins/jobs/simc-raid-reports/workspace/profiles"
 SIMC_OUTPUT_PATH="/simc/reports"
 
 ${SIMC_CLI_PATH}/simc Raid_T19P.simc iterations=${simc_iterations} html=${SIMC_OUTPUT_PATH}/Raid_T19P.html threads=${available_cpus} ${other_options} > ${SIMC_OUTPUT_PATH}/Raid_T19P.txt
-${SIMC_CLI_PATH}/simc Raid_T19H.simc iterations=${simc_iterations} html=${SIMC_OUTPUT_PATH}/Raid_T19H.html threads=${available_cpus} ${other_options} > #${SIMC_OUTPUT_PATH}/Raid_T19H.txt
-${SIMC_CLI_PATH}/simc Raid_T19M.simc iterations=${simc_iterations} html=${SIMC_OUTPUT_PATH}/Raid_T19M.html threads=${available_cpus} ${other_options} > #${SIMC_OUTPUT_PATH}/Raid_T19M.txt
+${SIMC_CLI_PATH}/simc Raid_T19H.simc iterations=${simc_iterations} html=${SIMC_OUTPUT_PATH}/Raid_T19H.html threads=${available_cpus} ${other_options} > ${SIMC_OUTPUT_PATH}/Raid_T19H.txt
+${SIMC_CLI_PATH}/simc Raid_T19M.simc iterations=${simc_iterations} html=${SIMC_OUTPUT_PATH}/Raid_T19M.html threads=${available_cpus} ${other_options} > ${SIMC_OUTPUT_PATH}/Raid_T19M.txt
 
 # Aoe Sim
 #aoe_options="enemy=enemy2 actions=auto_attack,damage=0 enemy=enemy3 actions=auto_attack,damage=0 enemy=enemy4 actions=auto_attack,damage=0"
