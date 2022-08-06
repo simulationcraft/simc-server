8eb95cde2cd6469ffed49fc3b76bdba9d83a2828
nyterage <nyterage@gmail.com>
2022-08-06 14:17:45 -0700

    Add S4 stack to raid report processing

--- jenkins/process_raid_reports.sh
+++ jenkins/process_raid_reports.sh
@@ -25,3 +25,9 @@ cp /simc/reports/${REPORT}.html /simc/reports/archive_html/${NOW}_${REPORT}.html
 gzip /simc/reports/archive_html/${NOW}_${REPORT}.html
 cp /simc/reports/${REPORT}.txt /simc/reports/archive_txt/${NOW}_${REPORT}.txt
 gzip /simc/reports/archive_txt/${NOW}_${REPORT}.txt
+
+REPORT=T28_5_Raid
+cp /simc/reports/${REPORT}.html /simc/reports/archive_html/${NOW}_${REPORT}.html
+gzip /simc/reports/archive_html/${NOW}_${REPORT}.html
+cp /simc/reports/${REPORT}.txt /simc/reports/archive_txt/${NOW}_${REPORT}.txt
+gzip /simc/reports/archive_txt/${NOW}_${REPORT}.txt
